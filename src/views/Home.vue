b07a4963bc5288e07484eb594b6a4d083243b890
upgradecode-dev <70087961+upgradecode-dev@users.noreply.github.com>
2023-01-05 13:13:54 -0300

    lll

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,30 @@
+
+<script setup>
+import { db } from "../firebase"
+import { collection, addDoc} from "firebase/firestore";
+
+import Sidebar from "@/components/Sidebar.vue"
+
+const agregarDatos = async () => {
+    console.log("Funcionando")
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
+            nombre: "Claudio"
+         
+        });
+         
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+}
+</script>
+
 <template>
     <div>
+    <Sidebar></Sidebar>
         <h1>Home</h1>
+        <button @click="agregarDatos" class="mt-10 bg-teal-600 px-2 py-1 text-white">Agregar</button>
     </div>
-</template>
+    </template>
+    
