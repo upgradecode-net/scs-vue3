f77d277690a605723de2d6304ca0065d62c7e444
upgradecode-dev <70087961+upgradecode-dev@users.noreply.github.com>
2023-01-05 13:14:08 -0300

    ,,,.

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div class="">
+    <ul>
+        <router-link to="/"><li>Home</li></router-link>
+        <router-link to="/login"><li>Login</li></router-link>
+    </ul>
+  </div>
+</template>
+ 
+
+<style>
+</style> 
