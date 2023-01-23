49dc9658db8f7cc43584d78cd6277ff1da52c5b8
Babayeen <cosheeee40@gmail.com>
2023-01-23 16:05:36 -0300

    preparando sistema modular de componentes

---
+++ src/components/Prueba.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <p>contenido de prueba</p>
+    </div>
+</template>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,16 @@
 <script setup>
- 
-
-import Sidebar from "../components/Sidebar.vue"
+    import Sidebar from "../components/Sidebar.vue"
+    import Prueba from "../components/prueba.vue"
 </script>
 
 <template>
-    <div class="flex">
+<div class="flex">
 
-        <sidebar/>
+    <sidebar />
 
-        
+    <main class="w-full p-3 bg-white">
+        <Prueba/>
+    </main>
 
-        <main class="w-full p-3 bg-white">
-     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, optio sunt. Vitae eligendi iure, error placeat quasi nam voluptate officiis, earum magni esse blanditiis eaque delectus voluptatum fugit vero praesentium.
-     Saepe, in officiis repellat quo magni aliquid veniam repellendus eum quisquam recusandae illum esse quas tempore! Repellendus odit dolores, asperiores possimus, aperiam molestiae reiciendis doloribus nihil eveniet deserunt consequuntur laborum.
-     Dicta possimus numquam ab quia? Sint, voluptatibus! Sequi voluptates qui praesentium nemo iste fuga a, error eius odit hic repellat. Doloremque sapiente assumenda illo fugit aliquam. Magnam expedita quod sint!
-     Magni deserunt molestiae, ullam corporis eaque iusto! Eius tempora animi facilis odio earum, reprehenderit, distinctio, beatae laborum quibusdam totam inventore ratione vitae maxime necessitatibus quos impedit doloribus et tenetur rerum.
-     Quae eos adipisci dolorum fugiat, ad minus, quis ratione at unde nesciunt, maiores illum. A, sint officia accusantium eos nemo aliquam consequatur necessitatibus est! Repudiandae cupiditate quaerat numquam perferendis expedita.
-     Totam voluptatem nulla quam culpa non! Nobis, voluptas eaque in animi tempore consequuntur, odio eligendi, ipsa explicabo modi deleniti consectetur recusandae minima inventore maxime facere dicta. Quidem, numquam obcaecati? Expedita.
-     Quis facilis iste nihil dolor nemo sunt labore amet unde ex accusamus. Libero voluptates ipsum eveniet laboriosam nemo! Repellat tempora eos dolorem quisquam qui nulla quam quas labore, officiis iste.
-     Error doloremque eveniet tenetur sequi officiis fugit pariatur ipsam commodi sit ex? Quae error delectus recusandae repellat iste ut repellendus ipsa sit asperiores deleniti? Nemo voluptas dolor quam ex sint!
-     Tenetur quaerat voluptate quod ipsum, alias expedita ea dicta, soluta sint provident error, vero omnis. Non quas maxime aliquam unde expedita earum incidunt, odit, nostrum exercitationem, veniam soluta similique voluptatibus?
-     Eum commodi rem ipsa dolore. Consequuntur unde assumenda, mollitia tempora distinctio eaque quisquam harum deleniti, rem quibusdam ipsam saepe quis amet? Obcaecati eum facere possimus? Facilis commodi exercitationem fuga omnis.
-     Consequatur ut unde, dolorem ad laboriosam placeat excepturi maxime magni a doloremque, possimus sequi voluptates provident fugiat praesentium asperiores vitae minus totam cupiditate recusandae, iste molestiae! Laborum illum aperiam optio!
-     Nobis recusandae corporis culpa voluptatem maiores non optio cupiditate quas. Atque quibusdam molestiae tempore iusto! Consectetur assumenda voluptates necessitatibus doloribus alias, dolorum blanditiis fugiat dolorem consequuntur similique commodi ad tempora?
-     Nulla, consectetur? Obcaecati nam consequatur harum earum, provident omnis. Earum debitis facilis corrupti architecto assumenda quos repudiandae obcaecati, dicta sit natus commodi aut enim deleniti asperiores tempore at voluptatem doloribus!
-     Quam beatae culpa pariatur necessitatibus fugit, cupiditate repudiandae quisquam provident nihil. Quod maxime amet illum impedit optio dolorum sed temporibus provident in ipsam maiores, molestias aspernatur, ullam recusandae dicta! Aliquam?
-     Eveniet nobis eaque delectus nemo incidunt quibusdam officia ducimus facere assumenda voluptates? Excepturi ducimus doloribus esse eius asperiores possimus maiores provident dolorem quo veniam? Velit obcaecati hic quidem architecto alias.
-     Perfer
-     nda quibusdam tempora aut, neque, fugiat minima architecto quis nesciunt amet corporis necessitatibus dolore fuga quo optio dolorum ratione veniam eligendi. Repellat, itaque.
-     Reprehenderit recusandae, atque eos illum facere error at tempora animi, eaque fuga eveniet dicta quae nulla, numquam ducimus repellendus nemo sed unde cupiditate harum commodi minima ipsum aut. Corrupti, quidem!
-     Quod ducimus rem commodi architecto sequi laborum et minima tempora vero ipsum ratione corporis distinctio quasi laudantium, amet, modi doloremque consequuntur praesentium illo quis vel voluptatum nostrum reprehenderit. Porro, nam?
-     Cum quasi accusantium exercitationem eveniet iste sed aliquam explicabo. Beatae dolores eligendi, laborum voluptatem error sed rerum esse iure harum perferendis quos facilis ab vitae est illo earum. Cumque, corporis.
-     Laboriosam cumque dolores officiis ut illum, quam quos earum molestias blanditiis saepe. Laborum beatae autem, laboriosam inventore, consequuntur unde vitae, molestias molestiae nulla incidunt minus! Ea odit obcaecati quo similique?
-     Suscipit, vel vitae facilis molestiae error culpa laborum aut. Eaque maxime similique deserunt magnam ipsa unde nostrum culpa et recusandae nihil ducimus quasi expedita, fugit consequatur tempore. Ea, voluptatibus debitis.
-     Nobis suscipit quis sit, dolorum esse iure magni quas vero voluptatum ratione dolor illum laborum pariatur ad. A, dolores sed fugit nemo mollitia maiores aliquam consequatur ex, officiis repellat suscipit.
-     Maxime dolorem quia nemo veniam amet odio, vel ipsam. Praesere. Numquam ducimus praesentium dolor consequatur, quia ex, laborum eveniet repellendus eius repellat quam at aliquid illo.</main>
-
-    </div>
+</div>
 </template>
